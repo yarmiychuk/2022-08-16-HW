aba5a245e961e1b408a0dab58e07a168a48fa627
Dmitry Yarmiychuk <yarmiychuk@gmail.com>
2022-08-20 00:46:08 +0300

    Min Sum app added

--- 56-Min-Sum/Program.cs
+++ 56-Min-Sum/Program.cs
@@ -39,13 +39,11 @@ void PrintLine(int[,] array, int lineIndex)
 int[,] CreateRandom2DArray(int rows, int columns, int minValue, int maxValue)
 {
     int[,] newArray = new int[rows, columns];
+
     for (int i = 0; i < rows; i++)
-    {
         for (int j = 0; j < columns; j++)
-        {
             newArray[i, j] = rnd.Next(minValue, maxValue + 1);
-        }
-    }
+
     return newArray;
 }
 
@@ -54,9 +52,7 @@ void Show2DArray(int[,] array)
     for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
-        {
             Console.Write(array[i, j] + " ");
-        }
         Console.WriteLine();
     }
 }
